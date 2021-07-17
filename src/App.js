0084fcdeca370203a0a5e4280fa382b7e09d0d6c
myamann <mehmetyaman331@gmail.com>
2021-07-17 18:55:50 +0300

    sidebar finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
 import './App.css';
+import {Sidebar} from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World!</h1>
-    </div>
+    <Sidebar/>
   );
 }
 
